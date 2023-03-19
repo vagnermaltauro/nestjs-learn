d129862e0b97c357e4b8bace0f3e4fd8966b242b
vagnermaltauro <vagnermaltauro@gmail.com>
2023-03-19 11:25:22 -0300

    feat: add ConfigModule to load environment variables from .env file and change port enviroment

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
+  ConfigModule.forRoot({
+    envFilePath: '.env',
+  });
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+  await app.listen(process.env.PORT_ENV || 3001);
 }
 bootstrap();
