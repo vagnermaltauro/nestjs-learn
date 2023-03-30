6af54c4f9d3c515f11bf23dd6bcb58a77a158fd8
vagnermaltauro <vagnermaltauro@gmail.com>
2023-03-30 20:40:48 -0300

    feat: add UserIdCheckMiddleware to handle user ID verification in UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,8 @@
 import { PrismaModule } from './../prisma/prisma.module';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserIdCheckMiddleware } from 'src/middlewares/user-id-check.middleware';
 
 @Module({
   imports: [PrismaModule],
@@ -9,4 +10,11 @@ import { UserService } from './user.service';
   providers: [UserService],
   exports: [],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserIdCheckMiddleware).forRoutes({
+      path: '/users/:id',
+      method: RequestMethod.ALL,
+    });
+  }
+}
