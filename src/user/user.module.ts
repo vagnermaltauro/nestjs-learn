9007fbe1f05645552eaeb56673ce3a1143f33053
vagnermaltauro <vagnermaltauro@gmail.com>
2023-04-15 16:24:32 -0300

    feat: import UserService in user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -8,7 +8,7 @@ import { UserIdCheckMiddleware } from 'src/middlewares/user-id-check.middleware'
   imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService],
-  exports: [],
+  exports: [UserService],
 })
 export class UserModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
