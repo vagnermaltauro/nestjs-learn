bc0e966432758851906c551e4ece302cbab92ceb
vagnermaltauro <vagnermaltauro@gmail.com>
2023-04-15 16:47:54 -0300

    feat: add authService

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
 import { AuthService } from './auth.service';
-import { UserService } from './../user/user.service';
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthLoginDTO } from './dto/auth-login.dto';
 import { AuthRegisterDTO } from './dto/auth-register.dto';
@@ -9,7 +8,6 @@ import { AuthResetDTO } from './dto/auth-reset.dto';
 @Controller('auth')
 export class AuthController {
   constructor(
-    private readonly userService: UserService,
     private readonly authService: AuthService
   ) {}
 
@@ -20,7 +18,7 @@ export class AuthController {
 
   @Post('register')
   async register(@Body() body: AuthRegisterDTO) {
-    return this.userService.create(body);
+    return this.authService.register(body);
   }
 
   @Post('forget')
