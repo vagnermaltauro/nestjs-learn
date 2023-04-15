4a189d03a6afc7891978172cb9622280b2775924
vagnermaltauro <vagnermaltauro@gmail.com>
2023-04-15 16:23:52 -0300

    feat: remove export UserService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { UserService } from './user/user.service';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
-  exports: [UserService],
+  exports: [],
 })
 export class AppModule {}
