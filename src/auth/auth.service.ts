a83b047f0a134b4bf5c8a76da01b0c083cd13f69
vagnermaltauro <vagnermaltauro@gmail.com>
2023-04-15 15:35:43 -0300

    chore: add auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly JWTService: JwtService,
+    private readonly prisma: PrismaService
+  ) {}
+
+  async createToken() {
+    // return this.JWTService.sign();
+  }
+
+  async checkToken(token: string) {
+    // return this.JWTService.verify();
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.users.findFirst({
+      where: {
+        email,
+        password,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('email our password is not correct!');
+    }
+
+    return user;
+  }
+
+  async forget(email: string) {
+    const user = await this.prisma.users.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('email is not correct!');
+    }
+
+    // TO DO: send email...
+
+    return true;
+  }
+
+  async reset(password: string, token: string) {
+    const id = 0;
+
+    await this.prisma.users.update({
+      where: {
+        id
+      },
+      data: {
+        password: password,
+      }
+    });
+  }
+}
