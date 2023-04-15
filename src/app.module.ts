b49f99c332c5c7e3342132e3bc2e4f3e9e91d793
vagnermaltauro <vagnermaltauro@gmail.com>
2023-04-15 15:36:40 -0300

    feat: import and export UserService on app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [UserService],
 })
 export class AppModule {}
