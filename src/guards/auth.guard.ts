e2a6ada8f03cbdba51b8a08e36458609a5980483
vagnermaltauro <vagnermaltauro@gmail.com>
2023-04-18 20:54:34 -0300

    feat: add guards

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private readonly authService: AuthService) {}
+
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const { authorization } = request.headers;
+
+    try {
+      const data = this.authService.checkToken((authorization ?? '').split(' ')[1]);
+
+      request.userToken = data;
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
