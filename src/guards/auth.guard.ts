08806a43f77b61c10a55536adc044b282a42b06f
vagnermaltauro <vagnermaltauro@gmail.com>
2023-04-24 20:14:46 -0300

    refactor: refactor AuthGuard to fetch user info in canActivate method

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,19 +1,24 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(
+      private readonly authService: AuthService,
+      private readonly userService: UserService
+  ) {}
 
-  constructor(private readonly authService: AuthService) {}
-
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
     const { authorization } = request.headers;
 
     try {
-      const data = this.authService.checkToken((authorization ?? '').split(' ')[1]);
+      const data = await this.authService.checkToken((authorization ?? '').split(' ')[1]);
 
       request.userToken = data;
+
+      request.user = await this.userService.listOne(data.id);
 
       return true;
     } catch (e) {
